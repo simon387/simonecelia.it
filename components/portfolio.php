62ca45af7f5f86de6dbb0e86cf9da34d49be5a61
simone <simon387@hotmail.it>
2021-11-04 16:16:44 +0100

    end_portfolio

--- components/portfolio.php
+++ components/portfolio.php
@@ -1,44 +1,119 @@
 <section id="work" class="portfolio-mf sect-pt4 route">
-		<div class="container">
-				<div class="row">
-						<div class="col-sm-12">
-								<div class="title-box text-center">
-										<h3 class="title-a">
-												Portfolio
-										</h3>
-										<p class="subtitle-a">
-												Just few examples of my projects
-										</p>
-										<div class="line-mf"></div>
-								</div>
-						</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="title-box text-center">
+					<h3 class="title-a">
+						Portfolio
+					</h3>
+					<p class="subtitle-a">
+						Just few examples of my projects
+					</p>
+					<div class="line-mf"></div>
 				</div>
-				<div class="row">
-						<div class="col-md-4">
-								<div class="work-box">
-										<a href="img/work-1.jpg" data-lightbox="gallery-mf">
-												<div class="work-img">
-														<img src="img/work-1.jpg" alt="" class="img-fluid">
-												</div>
-												<div class="work-content">
-														<div class="row">
-																<div class="col-sm-8">
-																		<h2 class="w-title">Lorem impsum dolor</h2>
-																		<div class="w-more">
-																				<span class="w-ctegory">Web Design</span> / <span
-																						class="w-date">18 Sep. 2018</span>
-																		</div>
-																</div>
-																<div class="col-sm-4">
-																		<div class="w-like">
-																				<span class="ion-ios-plus-outline"></span>
-																		</div>
-																</div>
-														</div>
-												</div>
-										</a>
-								</div>
-						</div>
-				</div>
+			</div>
 		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="work-box">
+					<a href="img/cafe_placa.png" data-lightbox="gallery-mf">
+						<div class="work-img">
+							<img src="img/cafe_placa.png" alt="" class="img-fluid">
+						</div>
+						<div class="work-content">
+							<div class="row">
+								<div class="col-sm-8">
+									<h2 class="w-title">cafedelaplazalh.com</h2>
+									<div class="w-more">
+										<span class="w-ctegory">Web Design</span> / <span
+												class="w-date">23 Dec. 2020</span>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="w-like">
+										<span class="ion-ios-plus-outline"></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="work-box">
+					<a href="img/comarea.png" data-lightbox="gallery-mf">
+						<div class="work-img">
+							<img src="img/comarea.png" alt="" class="img-fluid">
+						</div>
+						<div class="work-content">
+							<div class="row">
+								<div class="col-sm-8">
+									<h2 class="w-title">comarealhocal.com</h2>
+									<div class="w-more">
+										<span class="w-ctegory">Web Design</span> / <span
+												class="w-date">25 Dec. 2020</span>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="w-like">
+										<span class="ion-ios-plus-outline"></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="work-box">
+					<a href="img/almacen.png" data-lightbox="gallery-mf">
+						<div class="work-img">
+							<img src="img/almacen.png" alt="" class="img-fluid">
+						</div>
+						<div class="work-content">
+							<div class="row">
+								<div class="col-sm-8">
+									<h2 class="w-title">Almacen</h2>
+									<div class="w-more">
+										<span class="w-ctegory">Warehouse Software</span> / <span
+												class="w-date">28 Mar. 2020</span>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="w-like">
+										<span class="ion-ios-plus-outline"></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="work-box">
+					<a href="img/daoc_starter.png" data-lightbox="gallery-mf">
+						<div class="work-img">
+							<img src="img/daoc_starter.png" alt="" class="img-fluid">
+						</div>
+						<div class="work-content">
+							<div class="row">
+								<div class="col-sm-8">
+									<h2 class="w-title">DaocStarter</h2>
+									<div class="w-more">
+										<span class="w-ctegory">Game Launcher Software</span> / <span
+												class="w-date">5 Oct. 2017</span>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="w-like">
+										<span class="ion-ios-plus-outline"></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
