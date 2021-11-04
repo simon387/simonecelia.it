8bdafcca2f080d690f365b00ffeb1a66f34dc327
simone <simon387@hotmail.it>
2021-11-04 12:23:14 +0100

    footer

--- components/footer.php
+++ components/footer.php
@@ -9,7 +9,7 @@
 														<div class="col-md-6">
 																<div class="title-box-2">
 																		<h5 class="title-left">
-																				Send Message Us
+																				Send Message
 																		</h5>
 																</div>
 																<div>
@@ -51,7 +51,7 @@
 																								</div>
 																						</div>
 																						<div class="col-md-12">
-																								<button type="submit" class="button button-a button-big button-rouded">
+																								<button disabled type="submit" class="button button-a button-big button-rouded">
 																										Send Message
 																								</button>
 																						</div>
@@ -67,29 +67,22 @@
 																</div>
 																<div class="more-info">
 																		<p class="lead">
-																				Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolorum dolorem
-																				soluta quidem
-																				expedita aperiam aliquid at.
-																				Totam magni ipsum suscipit amet? Autem nemo esse laboriosam ratione nobis
-																				mollitia inventore?
+																				This form is currently disabled, but feel free to contact me in other ways!
 																		</p>
 																		<ul class="list-ico">
-																				<li><span class="ion-ios-location"></span> 329 WASHINGTON ST BOSTON, MA 02108
-																				</li>
-																				<li><span class="ion-ios-telephone"></span> (617) 557-0089</li>
-																				<li><span class="ion-email"></span> contact@example.com</li>
+																				<li><span class="ion-ios-location"></span> ITALY and SPAIN</li>
+																				<li><span class="ion-ios-telephone"></span> (+39) 3403909232</li>
+																				<li><span class="ion-ios-telephone"></span> (+34) 644798608</li>
+																				<li><span class="ion-email"></span> simonecelia@simonecelia.it</li>
 																		</ul>
 																</div>
 																<div class="socials">
 																		<ul>
-																				<li><a href=""><span class="ico-circle"><i
-																												class="ion-social-facebook"></i></span></a></li>
-																				<li><a href=""><span class="ico-circle"><i
-																												class="ion-social-instagram"></i></span></a></li>
-																				<li><a href=""><span class="ico-circle"><i
-																												class="ion-social-twitter"></i></span></a></li>
-																				<li><a href=""><span class="ico-circle"><i
-																												class="ion-social-pinterest"></i></span></a></li>
+																				<li><a href="https://www.facebook.com/simone.celia.98/"><span class="ico-circle"><i class="ion-social-facebook"></i></span></a></li>
+																				<li><a href="https://www.instagram.com/_s1m0n387/"><span class="ico-circle"><i class="ion-social-instagram"></i></span></a></li>
+																				<li><a href="https://twitter.com/Simone_Celia"><span class="ico-circle"><i class="ion-social-twitter"></i></span></a></li>
+																				<li><a href="https://www.linkedin.com/in/simone-celia-b5b26a5/"><span class="ico-circle"><i class="ion-social-linkedin"></i></span></a></li>
+																				<li><a href="https://github.com/simon387/"><span class="ico-circle"><i class="ion-social-github"></i></span></a></li>
 																		</ul>
 																</div>
 														</div>
@@ -104,14 +97,8 @@
 						<div class="row">
 								<div class="col-sm-12">
 										<div class="copyright-box">
-												<p class="copyright">&copy; Copyright <strong>DevFolio</strong>. All Rights Reserved</p>
+												<p class="copyright">&copy; Copyright <strong>simonecelia.it</strong>. All Rights Reserved</p>
 												<div class="credits">
-														<!--
-															All the links in the footer should remain intact.
-															You can delete the links only if you purchased the pro version.
-															Licensing information: https://bootstrapmade.com/license/
-															Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=DevFolio
-														-->
 														Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
 												</div>
 										</div>
