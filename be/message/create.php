1c8cfeaa6189831dbdf8db015423a245d7faf1df
simone celia <simone.celia@thinkopen.it>
2021-11-05 18:12:34 +0100

    fix_and_test_prod

--- be/message/create.php
+++ be/message/create.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
@@ -11,11 +11,11 @@ include_once '../models/message.php';
 $database = new Database();
 $db = $database->getConnection();
 
-$post = new Message($db);
+$message = new Message($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
-if ($post->create($data->name, $data->email, $data->subject, $data->message)) {
+if ($message->create($data->name, $data->email, $data->subject, $data->message)) {
 	http_response_code(200);
 	echo json_encode(array("response" => "Message created"));
 } else {
