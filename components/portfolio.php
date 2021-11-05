a27314c6ebd8c8ebc97a7469d847cbf324911000
simone <simon387@hotmail.it>
2021-11-05 01:13:51 +0100

    port+1

--- components/portfolio.php
+++ components/portfolio.php
@@ -114,6 +114,31 @@
 					</a>
 				</div>
 			</div>
+			<div class="col-md-4">
+				<div class="work-box">
+					<a href="img/ytdl.png" data-lightbox="gallery-mf">
+						<div class="work-img">
+							<img src="img/ytdl.png" alt="" class="img-fluid">
+						</div>
+						<div class="work-content">
+							<div class="row">
+								<div class="col-sm-8">
+									<h2 class="w-title">YTDLUI</h2>
+									<div class="w-more">
+										<span class="w-ctegory">Desktop Application</span> / <span
+												class="w-date">7 Feb. 2017</span>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="w-like">
+										<span class="ion-ios-plus-outline"></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
