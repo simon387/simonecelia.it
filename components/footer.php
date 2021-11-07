0a3ff22ba22a2365267494843e0287f92c85d742
Simone Celia <simon387@hotmail.it>
2021-11-08 00:33:03 +0100

    dis

--- components/footer.php
+++ components/footer.php
@@ -20,7 +20,7 @@
 																						<div class="col-md-12 mb-3">
 																								<div class="form-group">
 																										<input type="text" name="name" class="form-control" id="name"
-																										       placeholder="Your Name" data-rule="minlen:4"
+																										       placeholder="Your Name" data-rule="minlen:4" disabled
 																										       data-msg="Please enter at least 4 chars"/>
 																										<div class="validation"></div>
 																								</div>
@@ -28,7 +28,7 @@
 																						<div class="col-md-12 mb-3">
 																								<div class="form-group">
 																										<input type="email" class="form-control" name="email" id="email"
-																										       placeholder="Your Email" data-rule="email"
+																										       placeholder="Your Email" data-rule="email" disabled
 																										       data-msg="Please enter a valid email"/>
 																										<div class="validation"></div>
 																								</div>
@@ -36,7 +36,7 @@
 																						<div class="col-md-12 mb-3">
 																								<div class="form-group">
 																										<input type="text" class="form-control" name="subject" id="subject"
-																										       placeholder="Subject" data-rule="minlen:4"
+																										       placeholder="Subject" data-rule="minlen:4" disabled
 																										       data-msg="Please enter at least 8 chars of subject"/>
 																										<div class="validation"></div>
 																								</div>
@@ -44,7 +44,7 @@
 																						<div class="col-md-12 mb-3">
 																								<div class="form-group">
 													<textarea class="form-control" name="message" rows="5"
-													          data-rule="required"
+													          data-rule="required" disabled
 													          data-msg="Please write something for us"
 													          placeholder="Message"></textarea>
 																										<div class="validation"></div>
