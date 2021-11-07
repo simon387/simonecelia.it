912c2b584aa5728beaef7402dd5cae8a20664ab8
Simone Celia <simon387@hotmail.it>
2021-11-07 13:39:30 +0100

    test push

--- components/nav.php
+++ components/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
 	<div class="container">
-		<a class="navbar-brand js-scroll" href="#page-top">SimoneCelia</a>
+		<a class="navbar-brand js-scroll" href="#page-top">SimoneCelia.it</a>
 		<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
 		        aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
 			<span></span>
