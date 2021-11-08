227de759ef4b5350e6ae53227aa52fce9f268870
simone <simon387@hotmail.it>
2021-11-08 18:30:04 +0100

    test

--- be/models/message.php
+++ be/models/message.php
@@ -1,15 +1,15 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
+
 class Message
 {
 	private $conn;
-	private $table_name = "message";
-	public $id;
-	public $name;
-	public $email;
-	public $subject;
-	public $message;
-	public $timestamp;
+	private string $table_name = "message";
 
 	public function __construct($db)
 	{
@@ -29,6 +29,36 @@ class Message
 
 		if ($stmt->execute()) {
 			//sendMail($name_, $email_, $subject_, $message_);
+			//Create an instance; passing `true` enables exceptions
+			$mail = new PHPMailer(true);
+
+			try {
+				//Server settings
+				$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+				$mail->isSMTP();                                            //Send using SMTP
+				$mail->Host       = 'mail.tophost.it';                     //Set the SMTP server to send through
+				$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+				$mail->Username   = 'webmaster@simonecelia.it';                     //SMTP username
+				$mail->Password   = '12Caratteri!';                               //SMTP password
+				$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+				$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+				//Recipients
+				$mail->setFrom('webmaster@simonecelia.it', 'Mailer');
+				$mail->addAddress('simone.celia@simonecelia.it', 'Joe User');     //Add a recipient
+				$mail->addReplyTo('simone.celia@simonecelia.it', 'Information');
+
+				//Content
+				$mail->isHTML(true);                                  //Set email format to HTML
+				$mail->Subject = 'Here is the subject';
+				$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+				$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+				$mail->send();
+				echo 'Message has been sent';
+			} catch (Exception $e) {
+				echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+			}
 			return true;
 		}
 		return false;
