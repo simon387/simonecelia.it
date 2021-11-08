e15b082c4ceb7feaf70e3a067dc0a1492b9ee4b7
Simone Celia <simon387@hotmail.it>
2021-11-08 02:07:57 +0100

    clean

--- be/models/message.php
+++ be/models/message.php
@@ -19,56 +19,13 @@ class Message
 	function create($name_, $email_, $subject_, $message_)
 	{
 		$query = "INSERT INTO " . $this->table_name .
-			 " (fromName, fromEmail, fromSubject, fromMessage) VALUES ('" . $name_ . "', '" . $email_ . "', '" . $subject_ . "', '" . $message_ ."')";
+			" (fromName, fromEmail, fromSubject, fromMessage) VALUES (:fromName, :fromEmail, :fromSubject, :fromMessage)";
 		$stmt = $this->conn->prepare($query);
 
 		$stmt->bindParam(":fromName", $name_);
 		$stmt->bindParam(":fromEmail", $email_);
 		$stmt->bindParam(":fromSubject", $subject_);
 		$stmt->bindParam(":fromMessage", $message_);
-
-		if ($stmt->execute()) {
-			return true;
-		}
-		return false;
-	}
-
-	function read($id_)
-	{
-		$query = "select m.message from " . $this->table_name . " m where m.id=:id_ ";
-		$stmt = $this->conn->prepare($query);
-
-		$stmt->bindParam(":id_", $id_);
-
-		$stmt->execute();
-		return $stmt;
-	}
-
-	function readAll()
-	{
-		$query = "select m.id, m.timestamp, m.message from " . $this->table_name . " m  order by m.timestamp desc";
-		$stmt = $this->conn->prepare($query);
-		$stmt->execute();
-		return $stmt;
-	}
-
-	function deleteAll()
-	{
-		$query = "TRUNCATE TABLE " . $this->table_name;
-		$stmt = $this->conn->prepare($query);
-
-		if ($stmt->execute()) {
-			return true;
-		}
-		return false;
-	}
-
-	function delete($id_)
-	{
-		$query = "delete from " . $this->table_name . " where id=:id_";
-		$stmt = $this->conn->prepare($query);
-
-		$stmt->bindParam(":id_", $id_);
 
 		if ($stmt->execute()) {
 			return true;
