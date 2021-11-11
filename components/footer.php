a53e4931c41dbc2b6fdff094963dc86591a36a13
simone celia <simone.celia@thinkopen.it>
2021-11-12 00:39:48 +0100

    _blank_on_link

--- components/footer.php
+++ components/footer.php
@@ -1,109 +1,114 @@
 <section class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(img/overlay-bg.jpg)">
-		<div class="overlay-mf"></div>
-		<div class="container">
-				<div class="row">
-						<div class="col-sm-12">
-								<div class="contact-mf">
-										<div id="contact" class="box-shadow-full">
-												<div class="row">
-														<div class="col-md-6">
-																<div class="title-box-2">
-																		<h5 class="title-left">
-																				Send Message
-																		</h5>
-																</div>
-																<div>
-																		<form action="" method="post" role="form" class="contactForm">
-																				<div id="sendmessage">Your message has been sent. Thank you!</div>
-																				<div id="errormessage"></div>
-																				<div class="row">
-																						<div class="col-md-12 mb-3">
-																								<div class="form-group">
-																										<input type="text" name="name" class="form-control" id="name"
-																										       placeholder="Your Name" data-rule="minlen:4"
-																										       data-msg="Please enter at least 4 chars"/>
-																										<div class="validation"></div>
-																								</div>
-																						</div>
-																						<div class="col-md-12 mb-3">
-																								<div class="form-group">
-																										<input type="email" class="form-control" name="email" id="email"
-																										       placeholder="Your Email" data-rule="email"
-																										       data-msg="Please enter a valid email"/>
-																										<div class="validation"></div>
-																								</div>
-																						</div>
-																						<div class="col-md-12 mb-3">
-																								<div class="form-group">
-																										<input type="text" class="form-control" name="subject" id="subject"
-																										       placeholder="Subject" data-rule="minlen:4"
-																										       data-msg="Please enter at least 8 chars of subject"/>
-																										<div class="validation"></div>
-																								</div>
-																						</div>
-																						<div class="col-md-12 mb-3">
-																								<div class="form-group">
+	<div class="overlay-mf"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="contact-mf">
+					<div id="contact" class="box-shadow-full">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="title-box-2">
+									<h5 class="title-left">
+										Send Message
+									</h5>
+								</div>
+								<div>
+									<form action="" method="post" role="form" class="contactForm">
+										<div id="sendmessage">Your message has been sent. Thank you!</div>
+										<div id="errormessage"></div>
+										<div class="row">
+											<div class="col-md-12 mb-3">
+												<div class="form-group">
+													<input type="text" name="name" class="form-control" id="name"
+													       placeholder="Your Name" data-rule="minlen:4"
+													       data-msg="Please enter at least 4 chars"/>
+													<div class="validation"></div>
+												</div>
+											</div>
+											<div class="col-md-12 mb-3">
+												<div class="form-group">
+													<input type="email" class="form-control" name="email" id="email"
+													       placeholder="Your Email" data-rule="email"
+													       data-msg="Please enter a valid email"/>
+													<div class="validation"></div>
+												</div>
+											</div>
+											<div class="col-md-12 mb-3">
+												<div class="form-group">
+													<input type="text" class="form-control" name="subject" id="subject"
+													       placeholder="Subject" data-rule="minlen:4"
+													       data-msg="Please enter at least 8 chars of subject"/>
+													<div class="validation"></div>
+												</div>
+											</div>
+											<div class="col-md-12 mb-3">
+												<div class="form-group">
 													<textarea class="form-control" name="message" rows="5"
 													          data-rule="required" id="message"
 													          data-msg="Please write something for us"
 													          placeholder="Message"></textarea>
-																										<div class="validation"></div>
-																								</div>
-																						</div>
-																						<div class="col-md-12">
-																								<button type="submit" class="button button-a button-big button-rouded">
-																										Send Message
-																								</button>
-																						</div>
-																				</div>
-																		</form>
-																</div>
-														</div>
-														<div class="col-md-6">
-																<div class="title-box-2 pt-4 pt-md-0">
-																		<h5 class="title-left">
-																				Get in Touch
-																		</h5>
-																</div>
-																<div class="more-info">
-																		<p class="lead">
-																				Feel free to contact me!
-																		</p>
-																		<ul class="list-ico">
-																				<li><span class="ion-ios-location"></span> ITALY and SPAIN</li>
-																				<li><span class="ion-ios-telephone"></span> (+39) 3403909232</li>
-																				<li><span class="ion-ios-telephone"></span> (+34) 644798608</li>
-																				<li><span class="ion-email"></span> simonecelia@simonecelia.it</li>
-																		</ul>
-																</div>
-																<div class="socials">
-																		<ul>
-																				<li><a href="https://www.facebook.com/simone.celia.98/"><span class="ico-circle"><i class="ion-social-facebook"></i></span></a></li>
-																				<li><a href="https://www.instagram.com/_s1m0n387/"><span class="ico-circle"><i class="ion-social-instagram"></i></span></a></li>
-																				<li><a href="https://twitter.com/Simone_Celia"><span class="ico-circle"><i class="ion-social-twitter"></i></span></a></li>
-																				<li><a href="https://www.linkedin.com/in/simone-celia-b5b26a5/"><span class="ico-circle"><i class="ion-social-linkedin"></i></span></a></li>
-																				<li><a href="https://github.com/simon387/"><span class="ico-circle"><i class="ion-social-github"></i></span></a></li>
-																		</ul>
-																</div>
-														</div>
+													<div class="validation"></div>
 												</div>
+											</div>
+											<div class="col-md-12">
+												<button type="submit" class="button button-a button-big button-rouded">
+													Send Message
+												</button>
+											</div>
 										</div>
+									</form>
 								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="title-box-2 pt-4 pt-md-0">
+									<h5 class="title-left">
+										Get in Touch
+									</h5>
+								</div>
+								<div class="more-info">
+									<p class="lead">
+										Feel free to contact me!
+									</p>
+									<ul class="list-ico">
+										<li><span class="ion-ios-location"></span> ITALY and SPAIN</li>
+										<li><span class="ion-ios-telephone"></span> (+39) 3403909232</li>
+										<li><span class="ion-ios-telephone"></span> (+34) 644798608</li>
+										<li><span class="ion-email"></span> simonecelia@simonecelia.it</li>
+									</ul>
+								</div>
+								<div class="socials">
+									<ul>
+										<li><a target="_blank" href="https://www.facebook.com/simone.celia.98/"><span class="ico-circle"><i
+															class="ion-social-facebook"></i></span></a></li>
+										<li><a target="_blank" href="https://www.instagram.com/_s1m0n387/"><span class="ico-circle"><i
+															class="ion-social-instagram"></i></span></a></li>
+										<li><a target="_blank" href="https://twitter.com/Simone_Celia"><span class="ico-circle"><i
+															class="ion-social-twitter"></i></span></a></li>
+										<li><a target="_blank" href="https://www.linkedin.com/in/simone-celia-b5b26a5/"><span
+														class="ico-circle"><i class="ion-social-linkedin"></i></span></a></li>
+										<li><a target="_blank" href="https://github.com/simon387/"><span class="ico-circle"><i
+															class="ion-social-github"></i></span></a></li>
+									</ul>
+								</div>
+							</div>
 						</div>
+					</div>
 				</div>
+			</div>
 		</div>
-		<footer>
-				<div class="container">
-						<div class="row">
-								<div class="col-sm-12">
-										<div class="copyright-box">
-												<p class="copyright">&copy; Copyright <strong>simonecelia.it</strong>. All Rights Reserved</p>
-												<div class="credits">
-														Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-												</div>
-										</div>
-								</div>
+	</div>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="copyright-box">
+						<p class="copyright">&copy; Copyright <strong>simonecelia.it</strong>. All Rights Reserved</p>
+						<div class="credits">
+							Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
 						</div>
+					</div>
 				</div>
-		</footer>
+			</div>
+		</div>
+	</footer>
 </section>
