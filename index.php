928b62970071032bfbb708fe5bd8e9d3e7fb47e6
simone <simon387@hotmail.it>
2021-11-12 17:38:56 +0100

    optimization

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include "components/head.php"; ?>
+<?php include "components/head.html"; ?>
 <body>
-<?php include "components/header.php"; ?>
-<?php include "components/hero.php"; ?>
+<?php include "components/header.html"; ?>
+<?php include "components/hero.html"; ?>
 <main id="main">
-	<?php include "components/about.php"; ?>
-	<?php include "components/services.php"; ?>
-	<?php include "components/counter.php"; ?>
-	<?php include "components/portfolio.php"; ?>
-    <!--?php include "components/testimonials.php"; ?-->
-    <!--?php include "components/blog.php"; ?-->
-	<?php include "components/contact.php"; ?>
+	<?php include "components/about.html"; ?>
+	<?php include "components/services.html"; ?>
+	<?php include "components/counter.html"; ?>
+	<?php include "components/portfolio.html"; ?>
+	<!--?php include "components/testimonials.php"; ?-->
+	<!--?php include "components/blog.php"; ?-->
+	<?php include "components/contact.html"; ?>
 </main>
-<?php include "components/footer.php"; ?>
+<?php include "components/footer.html"; ?>
 <div id="preloader"></div>
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-<?php include "components/script.php"; ?>
+<?php include "components/script.html"; ?>
 </body>
 </html>
