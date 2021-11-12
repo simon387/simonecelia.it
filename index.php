f55a33a31353106392ad138009be7e00e303a612
simone celia <simone.celia@thinkopen.it>
2021-11-12 01:30:01 +0100

    inizio_transazione_alla_4.6

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include "components/head.php"; ?>
+<?php include "components_old/head.php"; ?>
 <body id="page-top">
-<?php include "components/nav.php"; ?>
-<?php include "components/intro.php"; ?>
-<?php include "components/about.php"; ?>
-<?php include "components/services.php"; ?>
-<?php include "components/counters.php"; ?>
-<?php include "components/portfolio.php"; ?>
+<?php include "components_old/nav.php"; ?>
+<?php include "components_old/intro.php"; ?>
+<?php include "components_old/about.php"; ?>
+<?php include "components_old/services.php"; ?>
+<?php include "components_old/counters.php"; ?>
+<?php include "components_old/portfolio.php"; ?>
 <!--?php include "components/testimonials.php"; ?-->
 <!--?php include "components/blog.php"; ?-->
-<?php include "components/footer.php"; ?>
+<?php include "components_old/footer.php"; ?>
 <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 <div id="preloader"></div>
-<?php include "components/script.php"; ?>
+<?php include "components_old/script.php"; ?>
 </body>
 </html>
