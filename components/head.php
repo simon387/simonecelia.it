1ab9ec98dd0392c750343b3edc83767c701113cb
simone celia <simone.celia@thinkopen.it>
2021-11-12 01:30:01 +0100

    inizio_transazione_alla_4.6

--- components/head.php
+++ components/head.php
@@ -1,16 +1,14 @@
 <head>
 	<meta charset="utf-8">
-	<title>simonecelia.it</title>
 	<meta content="width=device-width, initial-scale=1.0" name="viewport">
-	<meta content="" name="keywords">
-	<meta content="" name="description">
-	<link href="img/favicon.ico" rel="icon">
-	<link href="img/apple-touch-icon.png" rel="apple-touch-icon">
-	<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-	<link href="lib/animate/animate.min.css" rel="stylesheet">
-	<link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
-	<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-	<link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
+	<title>SimoneCelia.it</title>
+	<meta content="simone celia homepage" name="description">
+	<meta content="simonecelia simone celia" name="keywords">
+	<link href="assets/img/favicon.ico" rel="icon">
+	<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+	<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+	<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet">
 </head>
