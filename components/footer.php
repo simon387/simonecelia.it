379f666ce6be85f84f4f415902d5b0f1a06060fb
simone celia <simone.celia@thinkopen.it>
2021-11-18 23:34:12 +0100

    clean_test

--- components/footer.php
+++ components/footer.php
@@ -4,14 +4,14 @@ $version = fgets($f);
 fclose($f);
 ?>
 <footer>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<div class="copyright-box">
-					<p class="copyright">&copy; Copyright <strong>simonecelia.it</strong>. All Rights Reserved. <?php echo $version ?>
-					<div class="credits">Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a></div>
-				</div>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="copyright-box">
+				<p class="copyright">&copy; Copyright <strong>simonecelia.it</strong>. All Rights Reserved. <?php echo $version ?>
+				<div class="credits">Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a></div>
 			</div>
 		</div>
 	</div>
+</div>
 </footer>
