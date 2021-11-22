4b5016478ffe4eec4fee150dc1ff7e9c53ad79a3
simone <simon387@hotmail.it>
2021-11-22 15:39:46 +0100

    1.3.19

--- index.php
+++ index.php
@@ -5,16 +5,14 @@
 <?php include "components/header.html"; ?>
 <?php include "components/hero.html"; ?>
 <main id="main">
-	<?php include "components/about.html"; ?>
-	<?php include "components/services.html"; ?>
-	<?php include "components/counter.html"; ?>
-	<?php include "components/portfolio.php"; ?>
-	<?php //testimonials, blog?>
-	<?php include "components/contact.html"; ?>
+<?php include "components/about.html"; ?>
+<?php include "components/services.html"; ?>
+<?php include "components/counter.html"; ?>
+<?php include "components/portfolio.php"; ?>
+<?php //testimonials, blog?>
+<?php include "components/contact.html"; ?>
 </main>
 <?php include "components/footer.php"; ?>
-<div id="preloader"></div>
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 <?php include "components/script.html"; ?>
 </body>
 </html>
