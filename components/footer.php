52addf8ba21c5ed751dbd940dc63ec414f411301
simone <simon387@hotmail.it>
2021-11-22 15:39:46 +0100

    1.3.19

--- components/footer.php
+++ components/footer.php
@@ -18,5 +18,7 @@ echo(minify_html(
 		</div>
 	</div>
 </div>
-</footer>'
+</footer>
+<div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>'
 ));
