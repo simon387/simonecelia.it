1a3189c9c8271ca6c7efbfa4eb10a15258d01ee1
simone <simon387@hotmail.it>
2022-02-04 03:15:06 +0100

    unselectable

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
 <header id="header" class="fixed-top">
 	<div class="container d-flex align-items-center justify-content-between">
 		<h1 class="logo"><a href="#"><?=TITLE ?></a></h1>
-		<nav id="navbar" class="navbar">
+		<nav id="navbar" class="navbar unselectable">
 			<ul>
 				<li><a class="nav-link scrollto active" href="#hero">Home</a></li>
 				<li><a class="nav-link scrollto" href="#about">About</a></li>
