d2714356517ece8f85e5917e4b6e952c2813ab29
simone <simone.celia@csi.it>
2022-03-24 13:07:38 +0100

    1.3.52

--- util/constants.php
+++ util/constants.php
@@ -1,6 +1,6 @@
 <?php
 const TITLE = "simonecelia.it";
 const EMAIL = "simone.celia@simonecelia.it";
-const PHONE1 = "(+39) 3403909232";
-const PHONE2 = "(+34) 644798608";
+const PHONE1 = "+39 3403909232";
+const PHONE2 = "+34 644798608";
 const LINKEDIN = "https://www.linkedin.com/in/simone-celia-b5b26a5/";
