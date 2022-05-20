0027321c60abcf319c26eac099a6edd68d456bea
simone <simone.celia@csi.it>
2022-05-20 13:21:47 +0200

    1.4.0

---
+++ shame.php
@@ -0,0 +1,44 @@
+<?php include "util/constants.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "components/head.html"; ?>
+</head>
+<body>
+<h1>The Shame List</h1>
+<p>The following numbers are of stalkers, phishing programs, thieves, ecc...</p>
+<ul>
+	<li>+390965351016</li>
+	<li>+393334485891</li>
+	<li>+390226922744</li>
+	<li>+39092560392</li>
+	<li>+390916739176</li>
+	<li>+390812783835</li>
+	<li>+390874456029</li>
+	<li>+390309140467</li>
+	<li>+34944770313</li>
+	<li>+34910714529</li>
+	<li>+34662980902</li>
+	<li>+3903821620001</li>
+	<li>+393501452483</li>
+	<li>+34662980718</li>
+	<li>+393499300302</li>
+	<li>+3907211748054</li>
+	<li>+393509769070</li>
+	<li>+393895566593</li>
+	<li>+393349444139</li>
+	<li>+447979125404</li>
+	<li>+393402553423</li>
+	<li>+393381083460</li>
+	<li>+393915564312</li>
+	<li>+393912246518</li>
+	<li>+441159729696</li>
+	<li>+390279021118</li>
+	<li>+390884965072</li>
+	<li>+390235967214</li>
+	<li>+3906656449</li>
+	<li>912041600</li>
+	<li>+347675373665</li>
+</ul>
+</body>
+</html>
