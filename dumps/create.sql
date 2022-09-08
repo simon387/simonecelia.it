6d7b5b43900f458c53729cb803a39240d547f494
Simone Celia <simone.celia@simonecelia.it>
2022-09-08 13:12:51 +0200

    1.4.16

--- dumps/create.sql
+++ dumps/create.sql
@@ -6,16 +6,17 @@ create database simonecelia;
 create table message
 (
     id            int auto_increment primary key,
-    fromName      varchar(256)                          null,
-    fromEmail     varchar(256)                          null,
-    fromSubject   varchar(256)                          null,
-    fromMessage   varchar(8192)                         null,
+    fromName      varchar(256)                          not null,
+    fromEmail     varchar(256)                          not null,
+    fromSubject   varchar(256)                          not null,
+    fromMessage   varchar(8192)                         not null,
     fromTimestamp timestamp default current_timestamp() not null
 );
 
 create table client
 (
-    id            int auto_increment primary key,
-    ip            varchar(256),
+    id      int auto_increment primary key,
+    ip      varchar(256)                          not null,
+    details varchar(8192)                         not null,
     visited timestamp default current_timestamp() not null
 )
