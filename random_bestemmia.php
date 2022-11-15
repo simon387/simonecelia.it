6650d0c58b5e8dee557dbff706062a6e959d4293
Simone Celia <simone.celia@simonecelia.it>
2022-11-15 13:47:03 +0100

    rm_file_proxy

--- random_bestemmia.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/json;');
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET");
-echo file_get_contents('https://bestemmie.org/api/random/');
-exit();
