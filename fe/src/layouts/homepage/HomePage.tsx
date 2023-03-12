fb2f9eedad8e3750a353694f57882b0bfadad374
Simone Celia <simone.celia@simonecelia.it>
2023-03-12 20:45:07 +0100

    fix_homepage_script

--- fe/src/layouts/homepage/HomePage.tsx
+++ fe/src/layouts/homepage/HomePage.tsx
@@ -1,8 +1,15 @@
 import {Hero} from "../hero/Hero";
-import React from "react";
+import React, {useEffect} from "react";
 import {About} from "../about/About";
 
 export const HomePage = () => {
+
+	useEffect(() => {
+		const script = document.createElement("script");
+		script.src = "main.js";
+		script.async = true;
+		document.body.appendChild(script);
+	});
 
 	return (
 		<>
