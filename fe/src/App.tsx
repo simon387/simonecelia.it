fe34e8e3c831e7b51e80fecd390c4bef8b8bb599
Simone Celia <simone.celia@simonecelia.it>
2023-03-12 17:36:32 +0100

    add favs, shamelist, fix routing, redirect 404

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from "./layouts/navbar/Navbar";
-import {Hero} from "./layouts/hero/Hero";
+import {Redirect, Route, Switch} from "react-router";
+import {HomePage} from "./layouts/homepage/HomePage";
+import {Favs} from "./layouts/favs/Favs";
+import {ShameList} from "./layouts/shamelist/ShameList";
 
 export const App = () => {
 
 	return (
-		<>
-			<Navbar/>
-			<Hero/>
-		</>
+		<Switch>
+			<Route path='/' exact>
+				<Redirect to='/home'/>
+			</Route>
+			<Route path='/home'>
+				<Navbar/>
+				<HomePage/>
+			</Route>
+			<Route path='/favs' exact>
+				<Favs/>
+			</Route>
+			<Route path='/shame' exact>
+				<ShameList/>
+			</Route>
+			<Redirect to="/"/>
+		</Switch>
 	);
 }
