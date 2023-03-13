1e785512db9d84f728e67cef5829eb9a2fabc2c9
Simone Celia <simone.celia@simonecelia.it>
2023-03-13 21:55:15 +0100

    new_trAX

--- fe/public/main.js
+++ fe/public/main.js
@@ -282,7 +282,7 @@
 			headers: {'X-Requested-With': 'XMLHttpRequest'}
 		})
 			.then(response => {
-				if( response.ok ) {
+				if (response.ok) {
 					return response.text()
 				} else {
 					throw new Error(`${response.status} ${response.statusText} ${response.url}`);
@@ -308,56 +308,59 @@
 		thisForm.querySelector('.error-message').classList.add('d-block');
 	}
 
+	function sendInfo() {
+		const info = {
+			timeOpened: new Date(),
+			timezone: (new Date()).getTimezoneOffset() / 60,
+			pageon: window.location.pathname,
+			referrer: document.referrer,
+			previousSites: history.length,
+			browserName: navigator.appName,
+			browserEngine: navigator.product,
+			browserVersion1a: navigator.appVersion,
+			browserVersion1b: navigator.userAgent,
+			browserLanguage: navigator.language,
+			browserOnline: navigator.onLine,
+			browserPlatform: navigator.platform,
+			javaEnabled: navigator.javaEnabled(),
+			dataCookiesEnabled: navigator.cookieEnabled,
+			dataCookies1: document.cookie,
+			dataCookies2: decodeURIComponent(document.cookie.split(";")),
+			dataStorage: localStorage,
+			sizeScreenW: screen.width,
+			sizeScreenH: screen.height,
+			sizeDocW: document.width,
+			sizeDocH: document.height,
+			sizeInW: innerWidth,
+			sizeInH: innerHeight,
+			sizeAvailW: screen.availWidth,
+			sizeAvailH: screen.availHeight,
+			scrColorDepth: screen.colorDepth,
+			scrPixelDepth: screen.pixelDepth,
+		};
+		fetch('http://localhost/simonecelia.it/be/controllers/client.php', {
+			method: 'POST',
+			body: JSON.stringify({
+				details: JSON.stringify(info).toString(),
+			}),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8'
+			}
+		}).then(function (response) {
+			if (response.ok) {
+				return response.json();
+			}
+			return Promise.reject(response);
+		}).then(function (data) {
+			// console.log(data);
+		}).catch(function (error) {
+			// console.warn('Something went wrong.', error);
+		});
+	}
+
+	sendInfo();
+
 })()
 
-// document.addEventListener("DOMContentLoaded", init); TODO
 
-function init() {
-	const info = {
-		timeOpened: new Date(),
-		timezone: (new Date()).getTimezoneOffset() / 60,
-		pageon: window.location.pathname,
-		referrer: document.referrer,
-		previousSites: history.length,
-		browserName: navigator.appName,
-		browserEngine: navigator.product,
-		browserVersion1a: navigator.appVersion,
-		browserVersion1b: navigator.userAgent,
-		browserLanguage: navigator.language,
-		browserOnline: navigator.onLine,
-		browserPlatform: navigator.platform,
-		javaEnabled: navigator.javaEnabled(),
-		dataCookiesEnabled: navigator.cookieEnabled,
-		dataCookies1: document.cookie,
-		dataCookies2: decodeURIComponent(document.cookie.split(";")),
-		dataStorage: localStorage,
-		sizeScreenW: screen.width,
-		sizeScreenH: screen.height,
-		sizeDocW: document.width,
-		sizeDocH: document.height,
-		sizeInW: innerWidth,
-		sizeInH: innerHeight,
-		sizeAvailW: screen.availWidth,
-		sizeAvailH: screen.availHeight,
-		scrColorDepth: screen.colorDepth,
-		scrPixelDepth: screen.pixelDepth,
-	};
-	fetch('be/controllers/client.php', {
-		method: 'POST',
-		body: JSON.stringify({
-			details: JSON.stringify(info).toString(),
-		}),
-		headers: {
-			'Content-type': 'application/json; charset=UTF-8'
-		}
-	}).then(function (response) {
-		if (response.ok) {
-			return response.json();
-		}
-		return Promise.reject(response);
-	}).then(function (data) {
-		// console.log(data);
-	}).catch(function (error) {
-		// console.warn('Something went wrong.', error);
-	});
-}
+
