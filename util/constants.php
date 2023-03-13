882b4b5dc0465253cc62d363d1f6771970c2e85b
simone.celia <simone.celia@eng.it>
2023-03-13 22:48:54 +0100

    refa

--- util/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once 'components/util/minify_html.php';
-
-const TITLE = "simonecelia.it";
-const EMAIL = "simone.celia@simonecelia.it";
-const PHONE1 = "+39 3403909232";
-const PHONE2 = "+34 644798608";
-const LINKEDIN = "https://www.linkedin.com/in/simone-celia-b5b26a5/";
