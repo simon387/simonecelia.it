58866e7fd7bf7c230ae6f62722890c196ff011e4
simone.celia <simone.celia@eng.it>
2023-04-01 08:49:05 +0200

    1.5.13

--- src/layouts/HomePage/components/about/About.tsx
+++ src/layouts/HomePage/components/about/About.tsx
@@ -40,7 +40,7 @@ export const About = () => {
 										<div className="progress">
 											<div className="progress-bar" role="progressbar" style={{width: "90%"}} aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}></div>
 										</div>
-										<span>JAVASCRIPT</span><span className="pull-right"> 90%</span>
+										<span>Javascript</span><span className="pull-right"> 90%</span>
 										<div className="progress">
 											<div className="progress-bar" role="progressbar" style={{width: "90%"}} aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}></div>
 										</div>
