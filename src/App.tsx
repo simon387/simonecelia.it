bee481afeeea8b99fad8cb7df57ed909a5fa7c7c
Simone Celia <simone.celia@simonecelia.it>
2023-04-26 19:01:18 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from "./layouts/Navbar/Navbar";
-import {HashRouter} from "react-router-dom";
+import {HashRouter, Route} from "react-router-dom";
 import {HomePage} from "./layouts/HomePage/HomePage";
 import {Favs} from "./layouts/Favs/Favs";
 import {ShameList} from "./layouts/ShameList/ShameList";
 import {Footer} from "./layouts/Footer/Footer";
-import {Route} from "react-router";
 
 export const App = () => {
 
