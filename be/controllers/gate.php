80c58ddf6b7ab3c09b38e30c57a4b3023a2e546f
Simone Celia <simone.celia@simonecelia.it>
2025-03-14 01:20:42 +0100

    1.5.30

--- be/controllers/gate.php
+++ be/controllers/gate.php
@@ -13,8 +13,16 @@ function handleError($message)
 }
 
 try {
+	$opts = [
+		'http' => [
+			'method' => 'GET',
+			'header' => 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+		]
+	];
+
+	$context = stream_context_create($opts);
 	// Prima chiamata: ottieni l'IP
-	$ipResponse = file_get_contents('www.simonecelia.it/ipapi/be/ip/read.php');
+	$ipResponse = file_get_contents('https://www.simonecelia.it/ipapi/be/ip/read.php', false, $context);
 
 	if (!$ipResponse) {
 		throw new Exception("Impossibile recuperare l'indirizzo IP");
@@ -25,7 +33,7 @@ try {
 
 	// Seconda chiamata: usa l'IP ottenuto per chiamare il proxy
 	$proxyUrl = "http://{$ipAddress}:8080/proxy";
-	$proxyResponse = file_get_contents($proxyUrl);
+	$proxyResponse = file_get_contents($proxyUrl, false, $context);
 
 	if (!$proxyResponse) {
 		throw new Exception("Impossibile contattare il proxy all'indirizzo {$proxyUrl}");
